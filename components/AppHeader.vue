04f0a79ba2da6fffad07787d15cbd3a102443168
Michael Verellino <verellino@gmail.com>
2020-07-03 20:15:17 +0700

    initial commit

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,49 +1,73 @@
 <template>
-    <header class="header">
-        <a class="logo">
-            <img src="../assets/logo.svg" alt="Mekel Verellino's Logo">
-        </a>
-        
-        <ul>
-            <li>
-                <nuxt-link to="/works">WORKS</nuxt-link>
-            </li>
-            <li>
-                <nuxt-link to="/about">ABOUT</nuxt-link>
-            </li>
-            <li>
-                <nuxt-link to="/contact">CONTACT</nuxt-link>
-            </li>
-        </ul>
-    </header>
+  <header class="header">
+    <nuxt-link to="/" class="logo">
+      <img src="../assets/logo.svg" alt="Mekel Verellino's Logo">
+    </nuxt-link>
+
+    <ul>
+      <li>
+        <nuxt-link to="/works">WORKS</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/about">ABOUT</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/contact">CONTACT</nuxt-link>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <script>
 export default {
-    name: 'AppHeader'
-}
+  name: "AppHeader"
+};
 </script>
 
 <style>
-.header{
-    position: fixed;
-    display: flex;
-    width: calc(100vw - 160px);
-    margin-top: 40px;
-    justify-content: space-between;
-    align-items: center;
-    z-index: 100;
-    mix-blend-mode: difference;
+.header {
+  position: fixed;
+  display: flex;
+  width: calc(100vw - 160px);
+  margin-top: 40px;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 100;
+  mix-blend-mode: difference;
 }
-.header ul{
-    display: flex;
+.header ul {
+  display: flex;
 }
-.header li{
-    display: inline-block;
-    padding: 0 1rem;
-    font-family: Rakkas;
+.header li {
+  display: inline-block;
+  padding: 0 1rem;
+  font-family: Rakkas;
 }
-.logo{
-    height: 40px;
+.logo {
+  height: 40px;
+}
+
+@media only screen and (min-device-width: 768px) and (max-device-width: 1280px) and (-webkit-min-device-pixel-ratio: 2) {
+  .header {
+    width: calc(100vw - 80px);
+  }
+}
+@media only screen and (min-device-width: 481px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+  .header {
+    width: calc(100vw - 32px);
+    padding-top: 16px;
+  }
+  .logo {
+    height: 32px;
+  }
+}
+@media only screen and (min-device-width: 300px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
+  .header {
+    width: calc(100vw - 16px);
+    padding-top: 8px;
+  }
+  .logo {
+    height: auto;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="text-align-center">
     <section class="hero full-height">
-      <span>MY FRIENDS CALL ME,</span>
-      <h1 class="outlines">MEKEL,<br>VERELLINO</h1>
-      <span>I DO WEB DESIGN & DEVELOPMENT.<br>
-            CALL ME A VISUAL DEVELOPER. SOUNDS LEGIT, RIGHT?</span>
+      <h1 class="outlines">MEKEL
+        <br>VERELLINO
+      </h1>
+      <span>I DO WEB DESIGN & FRONT-END DEVELOPMENT.
+        <br>CALL ME A VISUAL DEVELOPER. SOUNDS LEGIT, RIGHT?
+      </span>
     </section>
-    <section class="showcase full-height">
-      <h2 class="red-title left-align">RECENT WORKS</h2>
-      <flickity  class="flickity" ref="flickity" :options="flickityOptions">
-        <div class="carousel-cell">
-          <a href="http://disco-tique.com"></a>
-          <h2>DISCOTIQUE</h2>
+    <section class="showcase left-align">
+      <h2>RECENT WORKS</h2>
+      <div class="project-container">
+        <div class="project left-align">
+          <img class="project-img">
+          <h4>PROJECT NAME</h4>
+          <span>E COMMERCE / DIRECTION / UX</span>
         </div>
-      </flickity>
+        <div class="project left-align">
+          <img class="project-img">
+          <h4>PROJECT NAME</h4>
+          <span>E COMMERCE / DIRECTION / UX</span>
+        </div>
+        <div class="project left-align">
+          <img class="project-img">
+          <h4>PROJECT NAME</h4>
+          <span>E COMMERCE / DIRECTION / UX</span>
+        </div>
+      </div>
     </section>
     <section class="aboutme">
-      <div class="pma">
-        <img src="https://media.giphy.com/media/6bSoMak3jOH4s/giphy.gif" class="giphy-embed" alt="punk">
-        <h2 class="red-title">FOR THE LOVE OF PUNK ROCK</h2>
-        <h2 class="red-title">AND CHEAP BOOZE,</h2>
-        <h2 class="red-title">KEEP THE PMA!</h2>
-      </div>
       <div class="bio full-height">
         <!--<h1 class="outlines right-align">MICHAEL<br>VERELLINO</h1>-->
         <div class="bio-right">
@@ -30,17 +37,24 @@
         </div>
         <div class="bio-left">
           <h2 class="red-title">A BRIEF INQUIRY</h2>
+          <p class="hyphenate">IT'S PRONOUNCED /mɛk hɛl/.
+            <br>IT'S JAVANESE PRONUNCIATION, BUT IT IS SPELLED MICHAEL ON MY BIRTH CERTIFICATE 😜
+          </p>
           <br>
-          <p>IT'S PRONOUNCED /mɛk hɛl/. IT'S JAVANESE PRONUNCIATION DUDE.
-          <br>BASED IN YOGYAKARTA, INDONESIA.</p>
-          <br><br>
-          <p>🤙 SUP!
-            <br><br>
-            I'VE BEEN WORKING TOGETHER WITH CREATIVE MINDS ACROSS THIS PLANET CALLED EARTH. EXPLORING VISUALS IN THE WORLD WIDE WEB AND CREATING PERSONAS FOR THE TEAM OR INDIVIDUALS I'VE BEEN WORKING WITH. I HAVE BEEN FREELANCING SINCE 2014,
+          <p class="hyphenate">
+            🤙 SUP!
+            <br>
+            <br>I'M A HUMAN BEING WHO IS ALSO A FREELANCER BASED IN YOGYAKARTA, INDONESIA.
+            <br>
+            <br>I'VE BEEN WORKING TOGETHER WITH CREATIVE MINDS ACROSS THIS PLANET CALLED EARTH.
+            <br>
+            <br>EXPLORING VISUALS IN THE WORLD WIDE WEB AND CREATING PERSONAS FOR THE TEAM OR INDIVIDUALS. I HAVE BEEN FREELANCING SINCE 2014,
             ALWAYS AVAILABLE FOR CREATING NEW EXPERIENCES AND BUILDING VIRTUAL WORLDS.
-            <br><br> GET IN TOUCH, LET'S GET SUM BEER.</p>
+            <br>
+            <br>GET IN TOUCH, LET'S GET SUM BEER.
+          </p>
           <br>
-          <h3><a href="mailto:verellino@gmail.com?subject=WASSUP MEKEL!🤙"><fa :icon="faEnvelope" /> | | EMAIL ME!</a></h3>
+          <a href="mailto:verellino@gmail.com?subject=WASSUP MEKEL!🤙">LET'S JOIN FORCES!</a>
         </div>
       </div>
     </section>
@@ -48,146 +62,134 @@
       <h1 class="outlines">LET'S WORK TOGETHER</h1>
       <a href="/">@verellino</a>
       <div class="footer">
-        <div class="footer-logo">
-          <img src="../assets/shaka.svg" alt="shaka hand sign">
-        </div>
-        <span>© 2020 | ALL RIGHTS RESERVED.</span>
-        <span>MICHAEL VERELLINO</span>
+        <span>©2020 | ALL RIGHTS RESERVED.</span>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import { faEnvelope } from '@fortawesome/fontawesome-free-solid'
 export default {
   head() {
     return {
-      title: 'Mekel Verellino - Web Design & Development',
+      title: "Mekel Verellino - Web Design & Development",
       meta: [
         {
           hid: "description",
           name: "description",
-          content: "Mekel Verellino is a Freelance Web Designer and Developer from Yogyakarta, Indonesia."
+          content:
+            "Mekel Verellino is a Freelance Web Designer and Web Developer based in Yogyakarta, Indonesia. Focusing on UI UX Design, Front-end Development and Creative Direction."
         }
       ]
     };
-  },
-  data () {
-    return {
-      flickityOptions: {
-        initialIndex: 0,
-        prevNextButtons: true,
-        draggable: true,
-        wrapAround: true,
-        fade: true,
-      }
-    }
-  },
-  
-    computed: {
-      faEnvelope () {
-         return faEnvelope
-      }
-    },
+  }
 };
-
 </script>
 
 <style>
-.full-height{
-  height: 100vh;
+.full-height {
+  min-height: 100vh;
   display: flex;
-  flex-direction:column;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
-.flickity{
-  margin-top: 50px;
-  width: 95vw;
-  height: 80vh;
+.left-align {
+  align-items: start;
+  text-align: left;
 }
-.carousel{
-  position: relative;
+.showcase {
+  min-height: 100vh;
+  margin-bottom: 100px;
+}
+.project-container {
+  margin-top: 40px;
+  -ms-box-orient: horizontal;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -moz-flex;
+  display: -webkit-flex;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: start;
+  justify-content: space-between;
+}
+.project {
+  width: 48%;
+  display: block;
+  margin-bottom: 120px;
+}
+.project-img {
   width: 100%;
-  height: 100%;
+  height: 300px;
+  background: no-repeat cover center;
+  margin-bottom: 24px;
 }
-.carousel-cell {
-  width: 70%;
-  height: 100%;
-  margin-right: 50px;
-  background: linear-gradient(180deg, rgba(128, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0.5) 100%), url(../assets/Discotique-Fashion-Ecommerce.jpg);
-  background-size: cover;
-  transition: background ease-in 400ms;
-}
-.carousel-cell a {
-  width: 100%;
-  height: 100%;
-}
-.carousel-cell h2{
-  position: absolute;
-  bottom: 10px;
-  right: 5px;
-}
-.carousel-cell:active {
-  background: url(../assets/Discotique-Fashion-Ecommerce.jpg);
-  background-size: cover;
-  transition: background ease-in 400ms;
-}
-.flickity-page-dots{
-  bottom: -50px;
-}
-.dot.is-selected{
-  transition: ease-in 300ms;
-  background: #800000;
-}
-.pma{
+.pma {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 80vh;
 }
-.giphy-embed{
+.giphy-embed {
   width: 250px;
   margin-bottom: 20px;
 }
-.bio{
+.bio {
   display: flex;
   flex-direction: row-reverse;
   align-items: flex-start;
 }
-.bio-left{
-  width: 50%;
+.bio-left {
+  width: 60%;
   height: 100%;
   text-align: left;
-  padding-right: 20px;
 }
-.bio-right{
-  width: 50%;
+.bio-right {
+  width: 40%;
   display: flex;
-  flex-direction:column;
-  align-items:flex-end;
+  flex-direction: column;
+  align-items: flex-end;
   position: sticky;
   position: -webkit-sticky;
-  top: 50px;
+  top: 80px;
 }
-.ava{
-  width: 500px;
+.ava {
+  width: 75%;
   background: #000;
 }
-.contactme{
+.contactme {
   position: relative;
 }
-.footer{
+.footer {
   position: absolute;
   bottom: 0;
   display: flex;
-  flex-direction:column;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
-.footer-logo{
-  width: 40px;
+@media only screen and (min-device-width: 320px) and (max-device-width: 767px) and (-webkit-min-device-pixel-ratio: 2) {
+  .project {
+    width: 100%;
+  }
+  .showcase {
+    height: auto;
+  }
+  .bio {
+    flex-direction: column-reverse;
+    justify-content: center;
+  }
+  .bio-right {
+    width: 100%;
+    align-items: center;
+  }
+  .bio-left {
+    width: 100%;
+    margin-bottom: 24px;
+    padding: 0;
+  }
 }
 </style>
